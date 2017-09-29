67d8097995818b7548a3066741e0917d68462a12
Raghu Betina <raghu@firstdraft.com>
2017-09-29 12:58:39 +0000

    Direct association between users and friends_requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :received_friends_request,
+             :class_name => "FriendsRequest",
+             :foreign_key => "recipient_id",
+             :dependent => :destroy
+
   has_many   :friends_requests,
              :foreign_key => "sender_id",
              :dependent => :destroy
